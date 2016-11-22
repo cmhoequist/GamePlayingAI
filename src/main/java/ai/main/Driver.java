b2796f4298609fb388d566e72716adf4147f3acc
Moritz <cmhoequist@gmail.com>
2016-11-22 12:59:37 -0600

    I don't know why so many of these files are pretending to have been changed

--- src/main/java/ai/main/Driver.java
+++ src/main/java/ai/main/Driver.java
@@ -4,4 +4,5 @@ package ai.main;
  * Created by justinkirk on 11/22/16.
  */
 public class Driver {
+
 }
--- src/main/java/vishal/Sandbox.java
+++ src/main/java/vishal/Sandbox.java
@@ -4,5 +4,6 @@ package vishal;
  * Created by Moritz on 11/22/2016.
  * <p></p>
  */
-public class Sandbox {
+public class Sandbox{
+
 }
